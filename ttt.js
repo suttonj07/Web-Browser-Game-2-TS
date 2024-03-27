d5e5d00fda1fe0b1799565fd8953575bfb2502d8
suttonj07 <suttonj07@gmail.com>
2024-03-27 12:56:33 -0400

    x and o working, not switching though

--- ttt.js
+++ ttt.js
@@ -21,30 +21,43 @@ const winConditions = [
 let options = ["", "", "", "", "", "", "", "", ""]
 //placeholders for each cell
 let currentPlayer = "X";
-//this is to track whos up
+//this is to track whos up/ if i want o's how would i do that
 let running = false;
-//switch to true before starting game
+//switch to true before starting game/did anyway
 
-startGame();
 //need to create all the functions i need to run game below
-//below = forEach box we will add an event listener (even is "click" with a callback of box clicked) to each box (how it supposed to work)
-//below = try again with event listener on click invokes the try again
+//below = forEach box we will add an event listener (event is "click" with a callback of box clicked) to each box (how it supposed to work)
+//below = startOverBtn with event listener on click invokes the try again
 function startGame() {
     box.forEach(box => box.addEventListener("click", boxClicked))
     startOverBtn.addEventListener("click", tryAgain);
     playerTurn.textContent = `${currentPlayer} turn`;
+    running = true;
 }
+// shouldnt dollar sign be blue?
 
 function boxClicked() {
+    const boxIndex = this.getAttribute("boxIndex");
+    if(options[boxIndex] != "" || !running){
+        return;
+    }
 
+    updateBox(this, boxIndex);
+    changeTurn(); 
+    // activte to see if it works = change turn function above
+    whoWon();
 }
 //may have to change index to something else
 function updateBox(box, index ) {
+    options[index] = currentPlayer;
+    box.textContent = currentPlayer;
 
 }
-
-function changePlayer() {
-
+//changePlayer= we take current and set it to if current is = x  then we reassing current to o, otherwise its still x.
+//text will say current player, its there turn. we are invoking it to say its whoevers turn 
+function changeTurn() {
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusText.textContent = `${currentPlayer} turn`;
 }
 
 function whoWon() {
@@ -54,3 +67,5 @@ function whoWon() {
 function tryAgain() {
 
 }
+
+startGame();
