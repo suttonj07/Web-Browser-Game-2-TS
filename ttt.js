d091a2b4fa581fad5c10a4d28c7de3c74b3fc7c1
suttonj07 <suttonj07@gmail.com>
2024-04-03 18:33:46 -0400

    cleaned up comments

--- ttt.js
+++ ttt.js
@@ -1,11 +1,7 @@
-// first declare all variables ill need 
-
 const box = document.querySelectorAll(".box"); 
-// .box selects box class
 let playerTurn = document.querySelector("#playerTurn");
-// #playerTurn selects class
 const startOverBtn = document.querySelector("#startOverBtn");
-// calling the button
+// An array of possible ways to win. Each number is assoiciated with a box.
 const winConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -17,27 +13,13 @@ const winConditions = [
         [2, 4, 6]
 ];
 
-// need an array of how to actually win. boxes go left to right starting with 0
-
 let options = ["", "", "", "", "", "", "", "", ""]
-//placeholders for each cell
 let currentPlayer = "X";
-//this is to track whos up/ if i want o's how would i do that
 let running = false;
-//switch to true before starting game/did anyway
 let statusText;
-//status text is the text under the board
-// let wins = 0;
-// let losses = 0;
-// let ties = 0;
 let player1Count = 0
 let player2Count = 0
 let tieCount = 0
-//need to create all the functions i need to run game below
-//below = forEach box we will add an event listener (event is "click" with a callback of box clicked) to each box (how it supposed to work)
-//below = startOverBtn with event listener on click invokes the try again
-startGame();
-
 
 function startGame() {
     box.forEach(box => box.addEventListener("click", boxClicked))
@@ -45,7 +27,6 @@ function startGame() {
     playerTurn.textContent = `${currentPlayer} turn`;
     running = true;
 }
-// shouldnt dollar sign be blue?
 
 function boxClicked() {
     const boxIndex = this.getAttribute("boxIndex");
@@ -54,56 +35,46 @@ function boxClicked() {
     }
 
     updateBox(this, boxIndex);
-    // changeTurn(); 
-    // activte to see if it works = change turn function above
+  
     whoWon();
 }
-//may have to change index to something else/puts x and o in box
+
 function updateBox(box, index ) {
     options[index] = currentPlayer;
     box.textContent = currentPlayer;
 
 }
-//changePlayer= we take current and set it to if current is = x  then we reassing current to o, otherwise its still x.
-//text will say current player, its there turn. we are invoking it to say its whoevers turn 
+
 function changeTurn() {
     currentPlayer = (currentPlayer === "X") ? "O" : "X"; 
-    // make current player be x otherwise go back to o
-    // statusText = `${currentPlayer} turn`;
-//     console.log(playerTurn)
-//    console.log(currentPlayer, "currentPlayer")
+    // Making the current player x, if not, go back to o.
+
     if(currentPlayer === "O"){
         playerTurn.innerHTML = `${currentPlayer} turn`;
-        // console.log(playerTurn)
+       
     }else{
         currentPlayer === "X"
         playerTurn.innerHTML = `${currentPlayer} turn`;
-            // console.log(playerTurn)
+      
     }
-    
 }
 
-
-
-//committed before starting this seciton below in case of errors/
 function whoWon() {
     let gameWon = false;
-//the for loop is looping over the arrays/it checks the numbers 0 1 and 2 and checks the options array (all the "" above) if those 3 are not spaces amd same, someone won
-    //if no winner, it will check the next "" array/repeats for each set of win conditions
+
     for(let i = 0; i < winConditions.length; i++) {
         const condition = winConditions[i];
         const cellA = options[condition[0]];
         const cellB = options[condition[1]];
         const cellC = options[condition[2]];
-//if cell a equals empty or cell b equals empty or c equals empty then continue.
-        if(cellA == "" || cellB == "" || cellC == ""){
+
+    if(cellA == "" || cellB == "" || cellC == ""){
           continue;
         }
-        //if a b and c are equal then winner
-        if(cellA == cellB && cellB == cellC){
+     
+    if(cellA == cellB && cellB == cellC){
           gameWon = true;  
           break;
-          //breaks us out of for loop
         }
     }
     if(gameWon){
@@ -111,55 +82,37 @@ function whoWon() {
         running = false;
         if (currentPlayer === "X") {
             player1Count++ 
-            // console.log(player1Count)
+
             updateScoreboard();
         }
         if (currentPlayer === "O") {
             player2Count++ 
-            // console.log(currentPlayer, player2Count, "im hit")
+            
             updateScoreboard();
         }
     }
-    //else if there are spaces then tie
+    
     else if(!options.includes("")){
-        // statusText 
         playerTurn.innerHTML = `Tie`;
         running = false;
-        console.log(playerTurn, "player turn hit")
         tieCount++
         updateScoreboard();
-
-        // console.log("tie")
+  
     }
     else{
         changeTurn();
     }
-   
 }
-
-// function updateStatus() {
-//     currentPlayer = "X";
-//     statusText = `${currentPlayer} turn`;
-// }
 
 function tryAgain() {
     currentPlayer = "X";
     options = ["", "", "", "", "", "", "", "", ""];
-    // taking options to reset them to empty spaces
     statusText = `${currentPlayer} turn`;
-    // starts it over with current player being x
     box.forEach(box => box.textContent = "");
-    //for each box we will update it to an empty box
-    // updateStatus();
-//    let currentPlayer = "";
     running= true;
     playerTurn.innerHTML = `${currentPlayer} turn`;
 }
 
-
-
-//used chat gpt. still need to change names to match mine.
-//wouldnt text content be referring to the content within the scoreboard.
 function updateScoreboard() {
    let p1Score = document.getElementById('p1score');
    p1Score.innerHTML = player1Count
@@ -167,24 +120,7 @@ function updateScoreboard() {
    p2Score.innerHTML = player2Count
    let tieScore = document.getElementById('tiescore');
    tieScore.innerHTML = tieCount
-     // document.getElementById('p2score').textContent = playerO;
-    // document.getElementById('tiescore').textContent = ties;
-  }
-//   function updatewhoWon(result) {
-//     if (result === 'p1score') {
-//       playerX++;
-//     } else if (result === 'p2score') {
-//       playerO++;
-//     } else if (result === 'tie') {
-//       ties++;
-//     }
-//     updateScoreboard();
-//   }
+}
 
+startGame();
 
-//   make a read me
-
-//make a branch to work on these
-//try to get a scoreboard for x and o (wont fail over it but it will be nice to have) if x when increment x by 1 and if 0 wins increment it by 1
-
-//add music
